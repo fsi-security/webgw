393cf7b2db6606c1324cbe37901542aea9798b9f
fsi-security <90693041+fsi-security@users.noreply.github.com>
2021-09-17 10:48:15 +0900

    Initial Commit

---
+++ README.md
@@ -0,0 +1,303 @@
+# 오픈소스를 이용한 AWS Management Console 어카운트ID  접근제어
+
+#### 주의사항: 프록시를 통한 AWS Management Console 어카운트 ID 접근제어는 AWS에서 공식적으로 가이드하는 방식은 아닙니다. 어카운트 ID가 포함된 헤더 정보는 언제든 변경될 수 있으며, 이로인한 어카운트 ID 접근제어 누락은 이 코드를 사용하는 당사자에게 책임이 있습니다. 제공된 샘플은 프록시를 통한 어카운트 ID 접근제어에 대한 이해를 돕기 위한 것으로 각자의 사용 환경에 맞도록 접근제어 규칙이 반영되어야 합니다
+
+## 1.  mitmproxy 설치 및 실행
+
+### 1-1) 윈도우
+```
+1. mitmproxy 설치
+https://mitmproxy.org/
+Download Windows Installer
+
+2. mitmproxy 디폴트 인증서 설치 경로
+%HomePath%\.mitmproxy
+
+(참고) python3는 설치하지 않아도 됨
+```
+
+### 1-2) 맥
+```
+1. mitmproxy 설치
+1-1) brew가 설치된 경우
+brew install mitmproxy
+
+1-2) python3가 설치된 경우
+sudo pip3 install mitmproxy
+
+## 2. mitmproxy 디폴트 인증서 설치 경로
+~\.mitmproxy
+```
+
+## 2. 클라이언트 인증서 설치
+### 2-1) 인증서 다운로드
+프록시 설정 상태로 http://mitm.it/ 접속
+![cert01](https://user-images.githubusercontent.com/90693041/133710390-1e838fb2-79a6-42b6-bcf1-823ff6f9f47b.png)
+### 2-2) 윈도우에서 클라이언트 인증서 설치
+![cert02](https://user-images.githubusercontent.com/90693041/133710395-ac1423e0-4cd1-4476-b0ef-6ede99096b44.png)
+
+```
+(설치 명령)
+certutil.exe -addstore -user -f "Root" mitmproxy-ca-cert.pem
+```
+![cert_win_01](https://user-images.githubusercontent.com/90693041/133710398-b53b1ba9-d465-4009-8831-c5cd76cc38dc.png)
+![cert_win_02](https://user-images.githubusercontent.com/90693041/133710407-c92f0376-d2bd-4ae7-8f95-c2951357edb7.png)
+
+```
+(인증서 설치 확인)
+certmgr
+```
+![cert_win_03](https://user-images.githubusercontent.com/90693041/133710410-21077ea3-8e82-42d4-b728-5666ef0a4a3c.png)
+
+
+### 2-3) 맥에서 클라이언트 인증서 설치
+![cert_mac_01](https://user-images.githubusercontent.com/90693041/133710416-8ca31968-1d1b-45d2-91c5-23f5ff34b911.png)
+
+mitmproxy-ca-cert.pem 더블클릭
+```
+숨김파일 표시 shift + command + .
+```
+
+![cert_mac_02](https://user-images.githubusercontent.com/90693041/133710418-3423c7bb-504e-4277-91ab-59acb9eb5f8b.png)
+
+![cert_mac_03](https://user-images.githubusercontent.com/90693041/133710424-556f0e48-fdef-481a-a940-1a30c6574129.png)
+
+![cert_mac_04](https://user-images.githubusercontent.com/90693041/133710433-39630300-c3c6-4e0e-af20-26e4fd986a6b.png)
+
+![cert_mac_05](https://user-images.githubusercontent.com/90693041/133710441-7cde45a1-9a97-4855-9117-da1597d6159a.png)
+
+![cert_mac_06](https://user-images.githubusercontent.com/90693041/133710446-febad9e3-6534-4dce-99c9-748b47e4b49a.png)
+
+![cert_mac_07](https://user-images.githubusercontent.com/90693041/133710450-7d32067e-65eb-433e-8b60-9001e23a7c6e.png)
+
+```
+(설치 명령)
+sudo security add-trusted-cert -d -p ssl -p basic -k /Library/Keychains/System.keychain ~/.mitmproxy/mitmproxy-ca-cert.pem
+```
+
+
+## 3.  클라이언트에서 mitmproxy 연결
+
+### 3-1) 브라우저(크롬)에서 프록시 연결
+크롬 Extensions에서 Proxy SwitchySharp 설치
+
+![proxy01](https://user-images.githubusercontent.com/90693041/133710456-09756393-940c-416a-b737-c26a9a18332a.png)
+
+Proxy SwitchySharp에서 mitmproxy 연결 설정 정보 입력
+
+![proxy02](https://user-images.githubusercontent.com/90693041/133710460-25beea6e-c192-4604-b489-4954268b93d7.png)
+
+mitmproxy 프록시 연결 설정 
+
+![proxy03](https://user-images.githubusercontent.com/90693041/133710469-00c4e677-848d-4f92-9703-3ed085db9612.png)
+
+### 3-2) 커맨드창에서 프록시 연결
+윈도우에서 설정
+```
+set HTTP_PROXY=http://127.0.0.1:8080
+set HTTPS_PROXY=http://127.0.0.1:8080
+set AWS_CA_BUNDLE=mitmproxy-ca-cert.pem
+```
+
+맥에서 설정
+```
+export HTTP_PROXY=http://127.0.0.1:8080
+export HTTPS_PROXY=http://127.0.0.1:8080
+export AWS_CA_BUNDLE=mitmproxy-ca-cert.pem
+```
+
+### 3-3)Custom 인증서 생성
+```
+openssl genrsa -out cert.key 2048 
+# (Specify the mitm domain as Common Name, e.g. \*.google.com) 
+openssl req -new -x509 -key cert.key -out cert.crt 
+cat cert.key cert.crt > cert.pem
+
+Country Name (2 letter code) [XX]:KR 
+State or Province Name (full name) []:Seoul 
+Locality Name (eg, city) [Default City]:GangNam 
+Organization Name (eg, company) [Default Company Ltd]:MyCompany 
+Organizational Unit Name (eg, section) []:IT 
+Common Name (eg, your name or your server's hostname) []:mydomain.com
+Email Address []:admin@mydomain.com
+
+위의 명령으로 파일 세개가 생성됨
+cert.crt
+cert.key
+cert.pem
+
+cert.pem파일은 private key와 인증서가 포함된 것으로
+이 파일은 mitmproxy-ca.pem 으로 변경
+mitmproxy-ca.pem은 
+-----BEGIN CERTIFICATE-----
+-----END RSA PRIVATE KEY----- 
+-----BEGIN CERTIFICATE----- 
+-----END CERTIFICATE----- 
+
+여기서 인증서 영역만 복사하여, mitmproxy-ca-cert.pem으로 생성
+-----BEGIN CERTIFICATE----- 
+-----END RSA PRIVATE KEY-----
+```
+
+## 4. mitmproxy 실행
+```
+(mitmproxy) CLI기반 interactive mode
+(mitmweb) WEB기반 GUI
+(mitmdump) non-interactive terminal output
+(addon 실행) mitmdump -s hello.py
+```
+
+## 5. mitmproxy 동작 방식 이해
+![mitmproxy01](https://user-images.githubusercontent.com/90693041/133710473-f1bcdc51-8ddf-42be-8c53-c0facc420c46.png)
+https://docs.mitmproxy.org/stable/concepts-modes/
+
+## 6. mitmproxy addon 개발
+### 6-1) addon 샘플
+```
+# hello.py
+from mitmproxy import http
+from mitmproxy.script import concurrent
+
+#concurrent는 non-blocking mode
+@concurrent
+def request(flow):                  
+    host = flow.request.host
+    applog ("DEBUG", f"[REQUEST]{host}")
+
+    #Reponse 오버라이딩
+    if host == "www.google.com":
+        flow.response = http.Response.make(
+            200,  # (optional) status code
+            "<html><head></head><body>blocked</body></html>",  # (optional) content
+            {}  # (optional) headers
+        )
+
+def response(flow):
+    host = flow.request.host
+    content = flow.response.content
+    if host == "www.google.com":
+        applog ("DEBUG", f"[RESPONSE]{content}")
+
+def applog(file, msg):
+    ext = "log"
+    f = open(f"{file}.{ext}", 'a', encoding='utf-8')
+    f.write(f"{msg}\n")
+    f.close()
+```
+https://docs.mitmproxy.org/stable/addons-examples/
+
+### 6-2) request 트래픽에서 확인할 수 있는 주요 정보
+
+```
+flow.request.host                    #호스트: String
+flow.request.url                     #전체 URL: String
+flow.request.path                    #URI PATH: String
+flow.request.query                   #파라미터: Dictionary
+flow.request.headers                 #헤더: Dictionary
+flow.request.content                 #컨텐츠: String
+flow.request.urlencoded_form         #컨텐츠: Dictionary
+flow.client_conn.address             #클라이언트 IP:  List
+```
+
+## 7. AWS Management Console 트래픽 분석
+```
+<WEB>
+Root
+(전처리)
+https://signin.aws.amazon.com/signin 
+email: my@email.com
+(본처리)
+https://signin.aws.amazon.com/mfa 
+action: authenticateRoot 
+email: my@email.com
+
+IAM 
+https://signin.aws.amazon.com/authenticate 
+action: iam-user-authentication 
+account: 123412341234 
+username: awsstudent
+
+<AWS CLI>
+(b'Host', b's3.ap-northeast-2.amazonaws.com'), 
+(b'Accept-Encoding', b'identity'), 
+(b'User-Agent', b'aws-cli/1.20.26 Python/3.6.0 Windows/10 botocore/1.21.26'), 
+(b'X-Amz-Date', b'20210914T062657Z'), 
+(b'X-Amz-Content-SHA256', b'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'), 
+(b'Authorization', b'AWS4-HMAC-SHA256 Credential=AKIA1234567890ABCDEFG/20210914/ap-northeast-2/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=892396b204609be3ece9269a3ce8ddfc995f9f8a61391ac758262ebef030e208')]]
+
+<Boto3 - Python>
+(b'Host', b's3.ap-northeast-2.amazonaws.com'), 
+(b'Accept-Encoding', b'identity'), 
+(b'User-Agent', b'Boto3/1.18.41 Python/3.9.7 Windows/10 Botocore/1.21.41'), 
+(b'X-Amz-Date', b'20210914T062605Z'), 
+(b'X-Amz-Content-SHA256', b'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855'), 
+(b'Authorization', b'AWS4-HMAC-SHA256 Credential=AKIA1234567890ABCDEFG/20210914/ap-northeast-2/s3/aws4_request, SignedHeaders=host;x-amz-content-sha256;x-amz-date, Signature=9de33ae635a0fc9eee86ab5fb4da3dd83d37656549144cd1be33131c32f42d24')
+```
+
+
+## 8. 샘플코드
+https://github.com/fsi-security/webgw
+
+허용되지 않은 도메인 또는 IP로 접근 했을 때 Remote 서버로 데이터가 전송되지 않는 것에 대한 검증 과정 필수
+
+테스트 WEB서버의 Access 로그를 통해 확인할 수 있음
+
+### 8-1) webgw.py
+```
+AWS Management Console 어카운트ID 접근제어 샘플
+```
+
+### 8-2) allowaccount.txt
+```
+각 세그먼트는 한 라인을 하나의 아이템으로 인식하며, 빈 문자열로 구분
+
+[email]
+Root account 이메일
+
+[account]
+Root account id
+
+[accesskey]
+Access Key
+```
+
+### 8-3) allowlist.txt
+```
+한 라인에 하나의 도메인 입력
+- 시작 문자가 .일 경우 와일드카드 방식으로 처리되며, 해당 도메인이 포함된 host 허용
+- 시작 문자가 .이 아닐 경우 도메인과 동일한 host 허용
+```
+
+## 9. Custom Response 처리
+![console01](https://user-images.githubusercontent.com/90693041/133710479-5b20a38d-c47f-4fd8-a8ac-4c1817868d80.png)
+![console02](https://user-images.githubusercontent.com/90693041/133710481-9755704a-506e-489c-bec1-56714adf9841.png)
+
+
+## 10. 아마존리눅스에서 mitmrpoxy 설치 및 실행
+```
+1. python3 설치 확인
+python3 -V
+
+2. mitmproxy 설치
+pip3 install mitmproxy
+
+3. webgw 가져오기
+
+4. webgw 실행
+nohup mitmdump --set block_global=false -s webgw.py 1> /dev/null 2>&1 &
+
+(인증서 폴더 지정)
+nohup mitmdump --set block_global=false --set confdir=./cert_dir -s webgw.py &
+
+--set confdir 를 지정하지 않으면, .mitmproxy 폴더에서 인증서 찾음
+--set confdir에서 mitmproxy-ca-cert.pem 파일이 없으면 자동으로 생성함
+
+5. Request 로그 확인
+tail -f request.log 
+
+6. Audit 로그 확인
+tail -f account.log
+```
+
+## 11. 상용 솔루션 현황
+https://www.mcafee.com/enterprise/ko-kr/products/web-gateway.html
---
+++ webgw.py
@@ -0,0 +1,310 @@
+import time
+from mitmproxy.script import concurrent
+from mitmproxy import http
+from urllib.parse import unquote
+import json
+
+allowed_domain_list_file = "allowlist.txt"
+allowed_account_list_file = "allowaccount.txt"      
+authenticate_root_url = "signin.aws.amazon.com/signin"
+authenticate_root_action = "authenticateRoot"
+authenticate_iam_url = "signin.aws.amazon.com/authenticate"
+authenticate_iam_action = "iam-user-authentication"    
+allowed_domain_list = open(allowed_domain_list_file, 'r').read().split('\n')
+
+@concurrent
+def request(flow):
+
+    LOG_TAG = "request"
+    requestHost = flow.request.host
+    requestUrl = flow.request.url
+    clientIP = getClientIP(flow.client_conn.address)
+    customResponse = False
+    customHeader = ""
+    customBody = ""   
+
+    if checkAllowedDomain(requestHost) == True:
+        #허용된 도메인
+        applog(LOG_TAG, f"[{getCurrentTime()}][ALLOW]{clientIP} -> {requestHost}")
+
+        accountId = getAccountIdInUserInfo(getCookeyByKey(getHeaderByKey(flow, "Cookie"), "aws-userInfo"))
+        # 쿠키에 어카운트 ID가 있을 경우 검증
+        if accountId is not None:
+            customResponse, customBody, customHeader = checkAllowedAccountByAccountId(flow, accountId)
+
+        # signin url만 검증, root로 로그인
+        if authenticate_root_url in requestUrl:
+            customResponse, customBody, customHeader = checkAllowedAccountByRoot(flow)
+
+        # signin url만 검증, iam user로 로그인
+        elif authenticate_iam_url in requestUrl:
+            customResponse, customBody, customHeader = checkAllowedAccountByIAM(flow)
+
+        # cli url검증
+        elif ".amazonaws.com" in requestUrl:
+            customResponse, customBody, customHeader = checkAllowedAccountByConsole(flow)
+
+    else:
+        #허용되지 않은 도메인
+        applog(LOG_TAG, f"[{getCurrentTime()}][DENY]{clientIP} -> {requestHost}")
+        customResponse, customBody, customHeader = getDeniedDomainMessage(requestHost)        
+
+    #CustomReponse 오버라이딩
+    if customResponse == True:
+        flow.response = http.Response.make(
+            200,  # (optional) status code
+            customBody,  # (optional) content
+            customHeader  # (optional) headers
+        )
+
+def response(flow):
+
+    LOG_TAG = "response"    
+    # applog(LOG_TAG, f"[Response]{flow.response.text}")
+    # applog(LOG_TAG, f"[Response]{flow.response.headers}")
+
+def checkAllowedDomain(host):
+
+    for allowed_domain in allowed_domain_list:
+        if allowed_domain.strip() == "":
+            # 리스트에 blank 들어갔을 경우 allowlist가 동작하지 않는 것을 방지
+            pass
+        elif allowed_domain.startswith(".") == True and allowed_domain in host:
+            return True
+        elif allowed_domain == host:
+            return True
+
+    return False
+
+def checkAllowedAccountByAccountId(flow, account_id):
+
+    LOG_TAG = "account"
+    clientIP = getClientIP(flow.client_conn.address)
+    allowed_account = readAllowedToken('account')    
+
+    if account_id in allowed_account:
+        applog(LOG_TAG, f"[{getCurrentTime()}]{clientIP}:WEB:{account_id}:{flow.request.url}:ALLOW")
+        return (False, "", "")
+    else:
+        applog(LOG_TAG, f"[{getCurrentTime()}]{clientIP}:WEB:{account_id}:{flow.request.url}:DENY")
+        customResponse = True
+        customBody = b'<ForbiddenError><error>Not Allowed.</error></ForbiddenError>'
+        customHeader = {               
+            "Server": "Server",
+            "Date": "Tue, 14 Sep 2021 04:52:55 GMT",
+            "Content-Length": "60",
+            "x-amzn-RequestId": "fa95bc17-8e08-4ae1-88fe-0915960a00b0",
+            "Strict-Transport-Security": "max-age=47304000; includeSubDomains",
+            "Connection": "close"
+        }            
+        return (customResponse, customBody, customHeader)
+
+def checkAllowedAccountByRoot(flow):
+
+    LOG_TAG = "account"
+    action, email, password = ("", "", "")
+    clientIP = getClientIP(flow.client_conn.address)
+    allowed_email = readAllowedToken('email')        
+
+    # form item에서 필요한 정보 저장
+    for key, value in flow.request.urlencoded_form.items():
+        if key.lower() == "action":
+            action = value
+        elif key.lower() == "email":
+            email = value
+        elif key.lower() == "password":
+            password = value
+
+    #password 체크하는 이유는 email 로그인은 1.email검증 2.capcha검증 3.pw검증 순에서 3단계에 처리에서 에러 메세지를 처리할 수 있기 때문. 1에서는 메세지 처리 불가
+    if action.lower() == authenticate_root_action.lower() and len(password) > 0: 
+        # allow/deny 로깅을 위핸 inner if로 처리
+        if email not in allowed_email:
+            #deny log
+            applog(LOG_TAG, f"[{getCurrentTime()}]{clientIP}:ROOT:{email}::DENY")
+            customResponse = True
+            customBody = b'{"state":"FAIL","properties":{"Message":"You can use only allowed email.","Title":"AUTHENTICATION FAILURE"}}'
+            customHeader = {
+                "Content-Security-Policy": "default-src 'none' https://aws.amazon.com https://*.signin.aws.amazon.com https://signin.aws.amazon.com 'unsafe-inline'; img-src 'self' data: https://*.signin.aws.amazon.com https://signin.aws.amazon.com https://opfcaptcha-prod.s3.amazonaws.com https://images-na.ssl-images-amazon.com https://d1.awsstatic.com https://internal-cdn.amazon.com https://media.amazonwebservices.com https://d36cz9buwru1tt.cloudfront.net https://d0.awsstatic.com; media-src 'self' https://*.signin.aws.amazon.com https://signin.aws.amazon.com https://media.amazonwebservices.com https://d36cz9buwru1tt.cloudfront.net https://opfcaptcha-prod.s3.amazonaws.com; script-src 'self' https://aws.amazon.com https://*.signin.aws.amazon.com https://signin.aws.amazon.com https://d1dgtfo2wk29o4.cloudfront.net/fwcim.js https://m.media-amazon.com https://l0.awsstatic.com https://images-na.ssl-images-amazon.com 'unsafe-eval' 'unsafe-inline'; style-src 'self' https://aws.amazon.com https://*.signin.aws.amazon.com https://signin.aws.amazon.com https://aws-signin-website-assets.s3.amazonaws.com https://l0.awsstatic.com https://images-na.ssl-images-amazon.com 'unsafe-inline'; report-uri /metrics/cspreport;",
+                "Content-Type": "application/json;charset=utf-8",
+                "Content-Security-Policy-Report-Only": "frame-ancestors 'self'; report-uri /metrics/cspreportonly;",
+                "Server": "Server",
+                "Strict-Transport-Security": "max-age=31536000; includeSubDomains",
+                "Transfer-Encoding": "chunked",
+                "X-Content-Type-Options": "nosniff",
+                "X-Frame-Options": "DENY",
+                "X-UA-Compatible": "IE=Edge",
+                "X-XSS-Protection": "1; mode=block"
+            }
+
+            return (customResponse, customBody, customHeader)
+        else:
+            #allow log
+            applog(LOG_TAG, f"[{getCurrentTime()}]{clientIP}:ROOT:{email}::ALLOW")
+
+    return (False, "", "")
+
+def checkAllowedAccountByIAM(flow):
+
+    LOG_TAG = "account"
+    action, account, username = ("", "", "")
+    clientIP = getClientIP(flow.client_conn.address)
+    allowed_account = readAllowedToken('account')        
+
+    for key, value in flow.request.urlencoded_form.items():
+        if key.lower() == "action":
+            action = value
+        elif key.lower() == "account":
+            account = value
+        elif key.lower() == "username":
+            username = value
+
+    if action.lower() == authenticate_iam_action.lower() and len(username) > 0: # username은 추후 세부 접근통제를 위해
+        if account not in allowed_account:
+            #deny log
+            applog(LOG_TAG, f"[{getCurrentTime()}]{clientIP}:IAM:{account}:{username}:DENY")
+            customResponse = True
+            customBody = b'{"state":"FAIL","properties":{"result":"FAILURE","text":"You can use only allowed account."}}'
+            customHeader = {                    
+                "Accept": "application/json, text/plain, */*",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+                "Connection": "keep-alive",
+                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8",
+                "Host": "signin.aws.amazon.com",
+                "Origin": "https://signin.aws.amazon.com",
+                'sec-ch-ua': '"Chromium";v="92", " Not A;Brand";v="99", "Google Chrome";v="92"',
+                "sec-ch-ua-mobile": "?0",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin",
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Safari/537.36"
+            }            
+            return (customResponse, customBody, customHeader)
+        else:
+            #allow log
+            applog(LOG_TAG, f"[{getCurrentTime()}]{clientIP}:IAM:{account}:{username}:ALLOW")
+
+    return (False, "", "")
+
+def checkAllowedAccountByConsole(flow):
+
+    LOG_TAG = "account"
+    accessKey = ""
+    clientIP = getClientIP(flow.client_conn.address)
+    allowed_accesskey = readAllowedToken('accesskey')    
+
+    user_agent = getHeaderByKey(flow, "User-Agent").strip()    
+    authorization = getHeaderByKey(flow, "Authorization")
+    if (user_agent.upper().startswith("AWS-CLI") or user_agent.upper().startswith("BOTO3")) and authorization is not None:
+
+        accessKey = getAccessKey(authorization)
+
+        if accessKey not in allowed_accesskey:                
+            #deny log
+            applog(LOG_TAG, f"[{getCurrentTime()}]{clientIP}:CLI:{accessKey}:{flow.request.url}:DENY")
+            customResponse = True
+            customBody = b'<?xml version="1.0" encoding="UTF-8"?><Error><Code>NotAllowedAccessKeyId</Code><Message>The AWS Access Key Id you provided does not allowed in our systems.</Message><AWSAccessKeyId></AWSAccessKeyId><RequestId></RequestId><HostId></HostId></Error>'
+            customHeader = {}
+            return (customResponse, customBody, customHeader)
+        else:
+            #allow log
+            applog(LOG_TAG, f"[{getCurrentTime()}]{clientIP}:CLI:{accessKey}:{flow.request.url}:ALLOW")
+
+    return (False, "", "")
+
+def getDeniedDomainMessage(request_host):
+
+    customResponse = True                
+    customBodyStr = f'<html><head></head><body><b><font color="#FF0000">{request_host}</font></b> is not allowed.</body></html>'
+    customBody = customBodyStr.encode(encoding = "utf-8")        
+    customHeader = {}
+
+    return (customResponse, customBody, customHeader)
+
+def readAllowedToken(token):
+
+    tokenlist = []
+    itemlist = open(allowed_account_list_file, 'r').read().split('\n')
+    findToken = f"[{token}]"
+
+    token_index = itemlist.index(findToken)
+    for i in range(token_index, len(itemlist)):
+        if itemlist[i].strip() == "":
+            break
+        elif itemlist[i].strip() == findToken or itemlist[i].startswith("#"):
+            pass
+        else:
+            tokenlist.append(itemlist[i].strip())
+
+    return tokenlist
+
+def getHeaderByKey(flow, findkey):
+    for key, value in flow.request.headers.items(): 
+        if key.upper() == findkey.upper() and len(value) > 0:  
+            return value
+
+    return None
+
+def getAccountIdInUserInfo(userInfo):
+    if userInfo is not None:                    
+        decoded_userInfo = unquote(userInfo)
+        json_data = json.loads(decoded_userInfo)
+        user_arn = json_data['arn']        
+        if user_arn is not None:
+            items = user_arn.split(':')
+            # 데이터형식: arn:aws:iam::{ACCOUNT_ID}:root
+            if len(items) > 4:
+                return items[4]
+
+    return None
+
+def getCookeyByKey(cookie, findkey):
+    if cookie is not None:
+        items = cookie.split(';')
+        for item in items:
+            idx_delimiter = item.find("=")
+            if idx_delimiter >= 0:         
+                item_key = item[:idx_delimiter].strip()
+                item_value = item[idx_delimiter + 1:].strip()
+
+                if item_key == findkey:
+                    return item_value
+
+    return None
+
+def getAccessKey(auth_str):
+
+    findToken = "Credential="
+    idx_s = auth_str.find(findToken)
+    idx_e = auth_str.find("/", idx_s)
+    if idx_s < 0 or idx_e < 0:
+        return ""                
+
+    return auth_str[idx_s + len(findToken):idx_e]
+
+def applog(file, msg):
+    ext = "log"
+    f = open(f"{file}_{getLoggingFileTime()}.{ext}", 'a', encoding='utf-8')
+    f.write(f"{msg}\n")
+    f.close()
+
+def getClientIP(client_address):
+
+    client_ipv4 = ""
+    if len(client_address) > 0:
+        ip_info = client_address[0].split(':')
+        if len(ip_info) > 0:
+            if len(ip_info) > 3:                
+                client_ipv4 = ip_info[3]
+            else:
+                client_ipv4 = ip_info[0]               
+
+    return client_ipv4
+
+def getLoggingFileTime():    
+    now = time.localtime()
+    return "%04d%02d%02d%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour)
+
+def getCurrentTime():    
+    now = time.localtime()
+    return "%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec)
